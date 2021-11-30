56c3d16c8e209fa8c68843e9d1e26549b99c6967
Bruke Wondessen <bwondessen23@gmail.com>
2021-11-30 17:38:20 -0600

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Bruke on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
