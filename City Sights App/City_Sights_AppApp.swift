de5d64057e9eb36600495bbad0adf9fb7e71ae7b
Bruke Wondessen <bwondessen23@gmail.com>
2021-11-30 17:49:10 -0600

    - set up folders for views, models, and view models - rename default content view to launch view

--- City Sights App/City_Sights_AppApp.swift
+++ City Sights App/City_Sights_AppApp.swift
@@ -1,5 +1,5 @@
 //
-//  City_Sights_AppApp.swift
+//  City_Sights_App.swift
 //  City Sights App
 //
 //  Created by Bruke on 11/30/21.
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct City_Sights_AppApp: App {
+struct City_Sights_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
